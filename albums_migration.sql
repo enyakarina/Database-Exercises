d6140a2919ddd9a972a908111ecbc1fd35c57ad1
Karina <karina@alisons-MacBook-Air.local>
2016-05-06 11:19:17 -0500

    Created a table to store info about albums.

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) DEFAULT 'NONE',
+    record VARCHAR(100) NOT NULL,
+    release_date YEAR(4) NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
