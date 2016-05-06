0d5bdd1e3c106da8fb98c50c39cdad20e015e6af
Karina <karina@alisons-MacBook-Air.local>
2016-05-06 11:19:56 -0500

    Created different values to insert to the table.

---
+++ albums_seeder.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, record, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 48.1, 'pop,rock,R&B'),
+('AC/DC', 'Back in Black', 1980, 25.9, 'Hard Rock'),
+('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'Progressive Rock'),
+('Whitney House', 'The Bodyguard', 1992, 27.4, 'Soundtrack/R&B,soul,pop'),
+('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard rock, progressive rock'),
+('Eagles', 'Their Greatest Hits', 1976, 32.2, 'Rock, soft rock, folk rock'),
+('Bee Gees', 'Saturday Night Fever', 1977, 19, 'Disco'),
+('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock'),
+('Shania Twain', 'Come On Over', 1997, 29.6, 'Country, pop'),
+('Led Zeppelin', 'Led Zeppelin IV', 1971, 'Hard rock, heavy metal'),
+('Michael Jackson', 'Bad', 1987, 20.3, 'Pop, funk, rock');
